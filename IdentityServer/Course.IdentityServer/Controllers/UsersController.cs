b86897015bfaa99709343385fd176d7dd895c05d
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-07-16 22:38:48 +0300

    - IdentityServer için Users controller oluşturuldu. - SingUp Endpoint'i eklendi.

---
+++ IdentityServer/Course.IdentityServer/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using Course.IdentityServer.Dtos;
+using Course.IdentityServer.Models;
+using Course.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City,
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (result.Succeeded is false)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(e => e.Description).ToList(), 400));
+            }
+
+            return NoContent();
+        }
+    }
+}
