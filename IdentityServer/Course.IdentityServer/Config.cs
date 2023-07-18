dacc53d27b88c873858cc269830d5ed16ea9a054
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-07-18 22:21:40 +0300

    - Config Dsyasına IdentityResources  yetkilerini tanımladık. - Config Tarafında IdentityResources Tanımladığımız yetkilere göre client tarafındaki Clientın hangi yetkilere sahip olmasını tanımladık. - Client Tarafında token kurallarını oluşturdurk(Süresi kullanımı ve kullanım tekrarı gibi.)

--- IdentityServer/Course.IdentityServer/Config.cs
+++ IdentityServer/Course.IdentityServer/Config.cs
@@ -4,6 +4,7 @@
 
 using IdentityServer4;
 using IdentityServer4.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Course.IdentityServer
@@ -22,8 +23,19 @@ namespace Course.IdentityServer
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
-
+                       // Email OLmalı
+                       new IdentityResources.Email(),
+                       // Bunun Payload içerisinde olmak zorunda. SubKeyWord'un dolu olması gereklidir.
+                       new IdentityResources.OpenId(),
+                       // Kullanıcı Profile bilgilerine erişim sağlayabilsin.
+                       new IdentityResources.Profile(),
+                       // Custom Olarak Resource ekleme işlemi yapıyoruz. Yukarıdakiler tanımlı olan Resourceler toplu şekilde ekleme işlemi sağlanabiliyor.
+                       // JWT'nin payloadundaki kullanıcı bilgiler bizim için claim nesnesidir.
+                       // Burada Claim eklerken hangi Claim ile mapleneceğine karar veriyoruz 
+                       new IdentityResource(){Name = "roles", DisplayName = "Roles", Description = "User Roles", UserClaims = new []{"role"} },
                    };
+
+        // Client içerisinde bunun ekleme işlemide gerçekleştiriliyor.
 
         public static IEnumerable<ApiScope> ApiScopes =>
             new ApiScope[]
@@ -41,8 +53,34 @@ namespace Course.IdentityServer
                     ClientName = "Asp.Net Core MVC",
                     ClientId = "WebMvcClient",
                     ClientSecrets = { new Secret ("secret".Sha256())},
+                    // ClientCredential'da Refresh Token Yoktur.
                     AllowedGrantTypes = GrantTypes.ClientCredentials,
                     AllowedScopes = { "catalog_fullpermission", "photo_stock_fullpermission",IdentityServerConstants.LocalApi.ScopeName }
+                },
+                new Client
+                {
+                    ClientName = "Asp.Net Core MVC",
+                    ClientId = "WebMvcClientForUser",
+                    ClientSecrets = { new Secret ("secret".Sha256())},
+                    // Password' içerisinde bulunmaktadaır.
+                    // ResourceOwnerPasswordAndClientCredentials
+                    // Bunu tanımlamamamızın sebebi Password için refresh token kullanabilmemiz gerekmesidir. Tanımlanması durumunda kullanamayacağız.
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    // Burada hangi izinlere müsade edeceğimiz kısmını dolduruyoruz.
+                    // IdentityServerConstants.StandardScopes.OfflineAccess Refresh token için eklenmektedir. Kullanıcı offline olsa dahi kullanıcı adına yeni bir token alabiliriz istek atabiliriz. Burada elimizde refresh token olmadığı zaman Kullanıcıdan Email ve Password almak zorunda olmamak için buna ihtiyaç duyuyoruz.
+                    AllowedScopes = {IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, IdentityServerConstants.StandardScopes.OfflineAccess,"roles"},
+                     // Access Token süresini belirliyoruz. 1 saat olarak ayarlıyoruz.
+                     AccessTokenLifetime = 1*60*60,
+                     // Refresh Token süresini belirliyoruz.
+                     // TokenExpiration.Sliding => Refresh Token istendikçe AccessToken süresini otomatik artırır.
+                     // TokenExpiration.Absolute => Kesin Bir tarih verilmek istenirse kullanılır. 
+                     RefreshTokenExpiration = TokenExpiration.Absolute,
+                     // Absolute verdiğimiz için burada süresini belirtiyoruz.
+                     AbsoluteRefreshTokenLifetime = (int)(DateTime.Now.AddMonths(1) - DateTime.Now).TotalSeconds,
+                     // RefreshToken Birkeremi kullanılsın yoksa Arka Arkaya kullanılabilsin mi?
+                     // TokenUsage.ReUse => Tekrar Kullanılmasını sağlar..
+                     // Token.OneTimeOnly => Bir kere kullanılmasını sağlar.
+                     RefreshTokenUsage = TokenUsage.ReUse,
                 }
             };
     }
