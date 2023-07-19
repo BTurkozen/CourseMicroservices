7cc7b6847cbe84b5292abdeec51829d699de5cad
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-07-19 23:17:56 +0300

    - Basket Dtolarının oluşturulması işlemleri yapıldı.

---
+++ Basket/Course.Services.Basket/Dtos/BasketItemDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Course.Services.Basket.Dtos
+{
+    public sealed class BasketItemDto
+    {
+        public int Quantity { get; set; }
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
