04c024fb6aedcb3fb40e2aa80f6d0115c40eb3e6
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-07-19 22:39:40 +0300

    - PhotoStock Controller'ı yazıldı. - Save ve Delete actionları eklendi.

---
+++ Services/PhotoStock/Course.Service.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,51 @@
+﻿using Course.Service.PhotoStock.Dtos;
+using Course.Shared.ControllerBases;
+using Course.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Course.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> PhotoSave(IFormFile photoFile, CancellationToken cancellationToken)
+        {
+            if (photoFile is not null && photoFile.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Photos", photoFile.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+
+                await photoFile.CopyToAsync(stream, cancellationToken);
+
+                var returnPath = $"Photos/{photoFile.FileName}";
+
+                PhotoDto photoDto = new() { PhotoURL = returnPath };
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is Empty", 400));
+        }
+
+        public async Task<IActionResult> PhotoDelete(string photoURL)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Photos", photoURL);
+
+            if (System.IO.File.Exists(path) is false)
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found", 404));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
---
+++ Services/PhotoStock/Course.Service.PhotoStock/Dtos/PhotoDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Course.Service.PhotoStock.Dtos
+{
+    public sealed class PhotoDto
+    {
+        public string PhotoURL { get; set; }
+    }
+}
