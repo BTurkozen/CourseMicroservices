10ba09023fdc55cbd7472039ed36b98166d3373f
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-07-21 22:21:12 +0300

    - IDiscountService ve imzaları oluşturuldu. - DiscountDto'lar oluşturuldu.

---
+++ Services/Discount/Course.Services.Discount/Dtos/DiscountCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Course.Services.Discount.Dtos
+{
+    public class DiscountCreateDto
+    {
+        public string UserId { get; set; }
+        public int Rate { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ Services/Discount/Course.Services.Discount/Services/IDiscountService.cs
@@ -0,0 +1,17 @@
+﻿using Course.Services.Discount.Dtos;
+using Course.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Course.Services.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<Response<List<DiscountDto>>> GetAllAsync();
+        Task<Response<DiscountDto>> GetByIdAsync();
+        Task<Response<DiscountCreateDto>> SaveAsync(DiscountCreateDto discountCreateDto);
+        Task<Response<DiscountUpdateDto>> UpdateAsync(DiscountUpdateDto discountUpdateDto);
+        Task<Response<NoContent>> DeleteAsync(int id);
+        Task<Response<DiscountDto>> GetByCodeAndUserId(string code, string userId);
+    }
+}
