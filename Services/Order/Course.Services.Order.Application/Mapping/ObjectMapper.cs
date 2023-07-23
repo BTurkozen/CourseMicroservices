d696c0cbf1675e1a4c77e8d0850334e948293957
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-07-23 16:23:45 +0300

    - AutoMapper Application katmanına yüklendi. - Mapping için class oluşturularak dtolar maplendi. - ObjectMapper static class'ı oluşturuldu. Lazy method tanımlandı.

---
+++ Services/Order/Course.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course.Services.Order.Application.Mapping
+{
+    // Sadece ihtiyaç olduğunda yükleme işlemi gerçekleştirilecek.
+    public static class ObjectMapper
+    {
+        // Lazy İstendiğinde kullanılmasını sağlayan bir Class
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomMapping>();
+            });
+
+            return config.CreateMapper();
+        });
+
+        // Mapper çağrılana kadar Yukarıdaki lazy çalışmayacak. Taki nezamn Mapper çağrılırsa ozaman çalışmaya başlayacak.
+        public static IMapper Mapper => lazy.Value;
+    }
+}
