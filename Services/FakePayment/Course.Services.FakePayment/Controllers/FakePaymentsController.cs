f3caa4232f6b909e4fac28b1a7201c120a77cabd
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-07-23 21:00:01 +0300

    - FakePayments Controller oluşturuldu.

---
+++ Services/FakePayment/Course.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,19 @@
+﻿using Course.Shared.ControllerBases;
+using Course.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Course.Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> ReceviePayment()
+        {
+            return CreateActionResultInstance(Response<NoContentResult>.Success(200));
+        }
+    }
+}
