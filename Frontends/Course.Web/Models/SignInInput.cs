44a8973949d74bf71800a690b0e34cc702226de3
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-07-26 21:25:44 +0300

    - IIdentityservice eklendi ve imzaları oluşturuldu. - ServiceApiSetting nesnei oluşturuldu. IOption pattern ile api bilgileri alınacak. - Yüklenen bootstrap kütüphanesi layout içerisinde düzenlendi.

---
+++ Frontends/Course.Web/Models/SignInInput.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Course.Web.Models
+{
+    public sealed class SignInInput
+    {
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember")]
+        public bool IsRemember { get; set; }
+    }
+}
