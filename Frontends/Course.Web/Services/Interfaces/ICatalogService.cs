aef0b4316709ba61cd49b72d09f2d825bc4bf01f
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-07-29 23:46:37 +0300

    - CatalogService oluşturuldu.

--- Frontends/Course.Web/Services/Interfaces/ICatalogService.cs
+++ Frontends/Course.Web/Services/Interfaces/ICatalogService.cs
@@ -8,6 +8,7 @@ namespace Course.Web.Services.Interfaces
     public interface ICatalogService
     {
         Task<List<CourseViewModel>> GetAllCourseAsync();
+        Task<List<CategoryViewModel>> GetAllCategoryAsync();
         Task<List<CourseViewModel>> GetAllCourseByUserIdAsync(string userId);
         Task<CourseViewModel> GetByCourseIdAsync(string courseId);
         Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput);
