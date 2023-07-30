3eba6f46650189bcbae7b057858a09010ce1d08b
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-07-30 21:09:56 +0300

    - IBasketService ve imzaları oluşturuldu.

---
+++ Frontends/Course.Web/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,16 @@
+﻿using Course.Web.Models.BasketVMs;
+using System.Threading.Tasks;
+
+namespace Course.Web.Services.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<BasketViewModel> GetAllAsync();
+        Task<bool> SaveOrUpdateAsync(BasketViewModel basketViewModel);
+        Task<bool> DeleteAsync();
+        Task AddBasketItem(BasketItemViewModel basketItemViewModel);
+        Task<bool> RemoveBasketItem(string courseId);
+        Task<string> ApplyDiscount(string discountCode);
+        Task<bool> CancelApplyDiscount();
+    }
+}
