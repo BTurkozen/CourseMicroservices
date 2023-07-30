547985965077904df0ab39d237998a55284c4b49
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-07-30 21:05:09 +0300

    - BasketViewmModel ve BasketItemViewModel oluşturuldu discount işlemleri ve total price işlemleri için viewmodel içerisinde property genel ozellikleri kullanıldı.

---
+++ Frontends/Course.Web/Models/BasketVMs/BasketItemViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Course.Web.Models.BasketVMs
+{
+    public class BasketItemViewModel
+    {
+        public int Quantity { get; set; }
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+
+        private decimal? DiscountAppliedPrice { get; set; }
+        
+        public decimal GetCurrentPice { get => DiscountAppliedPrice != null ? DiscountAppliedPrice.Value : Price; }
+
+        public void AppliedDicount(decimal disCountPrice)
+        {
+            DiscountAppliedPrice = disCountPrice;
+        }
+    }
+}
---
+++ Frontends/Course.Web/Models/BasketVMs/BasketViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Course.Web.Models.BasketVMs
+{
+    public class BasketViewModel
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public int? DiscountRate { get; set; }
+        private List<BasketItemViewModel> _basketItems { get; set; }
+
+        public List<BasketItemViewModel> BasketItems
+        {
+            get
+            {
+                if (HasDiscount)
+                {
+                    _basketItems.ForEach(bi =>
+                    {
+                        var discountPrice = bi.Price * ((decimal)DiscountRate.Value / 100);
+                        bi.AppliedDicount(Math.Round(bi.Price - discountPrice, 2));
+                    });
+                }
+                return _basketItems;
+            }
+            set { _basketItems = value; }
+        }
+        public decimal TotalPrice { get => _basketItems.Sum(bi => bi.GetCurrentPice * bi.Quantity); }
+
+        public bool HasDiscount
+        {
+            get => !string.IsNullOrEmpty(DiscountCode);
+        }
+    }
+}
