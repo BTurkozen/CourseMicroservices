0d94097d34220df305f8d1e83c466e2c4b5491b4
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-01 22:05:43 +0300

    - IDiscountService ve Discount service yazıldı. - httpclient nesnesi olarak startup'a eklendiler ve app settings içerisinde gateway url leri eklendi.

---
+++ Frontends/Course.Web/Services/Concrates/DiscountService.cs
@@ -0,0 +1,22 @@
+﻿using Course.Web.Models.DiscountVMs;
+using Course.Web.Services.Interfaces;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Course.Web.Services.Concrates
+{
+    public class DiscountService : IDiscountService
+    {
+        private readonly HttpClient _httpClient;
+
+        public DiscountService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<DiscountViewModel> GetDiscountAsync(string code)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
