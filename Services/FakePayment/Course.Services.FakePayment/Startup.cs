ef326497637185c19d4feb2dc05cecf1248c77a0
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-05 21:38:09 +0300

    - Masstransit ve rabbitMq yüklendi. - Startup içerisinde ayarlamaları yapıldı.

--- Services/FakePayment/Course.Services.FakePayment/Startup.cs
+++ Services/FakePayment/Course.Services.FakePayment/Startup.cs
@@ -1,20 +1,14 @@
+﻿using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Course.Services.FakePayment
 {
@@ -30,6 +24,20 @@ namespace Course.Services.FakePayment
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMassTransit(ms =>
+            {
+                // Varsayılan Port : 5672
+                ms.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(Configuration["RabbitMQUrl"], "/", host =>
+                    {
+                        // RabitMq tarfından varsayılan olarak geliyor.
+                        host.Username("guest");
+                        host.Password("guest");
+                    });
+                });
+            });
+
             var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
 
             JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
