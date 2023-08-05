e87ae2908dd4ce25342d255c8b245b4820d46445
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-05 17:16:59 +0300

    - OrderController  içerisine checkout get işlemi ve view'ı oluşturuldu.

---
+++ Frontends/Course.Web/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Course.Web.Models.OrderVMs;
+using Course.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Course.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService;
+
+        public OrderController(IBasketService basketService, IOrderService orderService)
+        {
+            _basketService = basketService;
+            _orderService = orderService;
+        }
+
+        public async Task<IActionResult> Checkout()
+        {
+            ViewBag.basket = await _basketService.GetAllAsync();
+
+            return View(new CheckoutInfoInput());
+        }
+    }
+}
