302b8f4a993b4bb20990fa76a5b041612be2252f
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-08 22:21:26 +0300

    - TokenExchange handler startup içerisine eklendi. - Kullanılan HttpClient için startup içerisine service eklendi. - ocelot json dosyasında scopes discount ve fakepayment için kaldırılarak eklenen handler ismi eklendi.

--- Gateways/Course.Gateway/Startup.cs
+++ Gateways/Course.Gateway/Startup.cs
@@ -1,3 +1,4 @@
+using Course.Gateway.DelegateHandlers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -26,6 +27,8 @@ namespace Course.Gateway
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<TokenExchangeDelegateHandler>();
+
             services.AddAuthentication().AddJwtBearer("GatewayAuthenticationSchema", options =>
             {
                 options.Authority = Configuration["IdentityServerURL"];
@@ -33,7 +36,8 @@ namespace Course.Gateway
                 options.RequireHttpsMetadata = true;
             });
 
-            services.AddOcelot();
+            services.AddOcelot()
+                    .AddDelegatingHandler<TokenExchangeDelegateHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,16 +47,6 @@ namespace Course.Gateway
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            //app.UseRouting();
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World!");
-            //    });
-            //});
 
             app.UseOcelot();
         }
