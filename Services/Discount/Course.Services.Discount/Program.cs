d9420a86c668de47c943903c463a081dc8b975f9
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-13 15:08:04 +0300

    - Discount Microservice .net7'ye yükseltildi.

--- Services/Discount/Course.Services.Discount/Program.cs
+++ Services/Discount/Course.Services.Discount/Program.cs
@@ -1,26 +1,60 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Course.Services.Discount.Services;
+using Course.Shared.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
 
-namespace Course.Services.Discount
+var builder = WebApplication.CreateBuilder(args);
+
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+builder.Services.AddControllers(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+
+builder.Services.AddScoped<IDiscountService, DiscountService>();
+
+// Sadece read veya write yetkilendirme yapmak istediğimizde burada yeni bir policy oluşturuyoruz.
+//new AuthorizationPolicyBuilder().RequireClaim("discount_read");
+// Burada Tanımladığımız Policy'i Filter olarak controller içerisine ekliyoruz.
+// Filterlar birden fazla eklenebiliyor.
+
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.Authority = builder.Configuration["IdentityServerURL"];
+            options.Audience = "resource_discount";
+            options.RequireHttpsMetadata = true;
+        });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
