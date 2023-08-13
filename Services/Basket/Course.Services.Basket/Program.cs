35465fe07a009ca032e93f287a0abc70e3b5516b
Burak TÜRKÖZEN <bkturkozen@gmail.com>
2023-08-13 14:17:43 +0300

    - basket microservis .net 7 olarak yükseltildi.

--- Services/Basket/Course.Services.Basket/Program.cs
+++ Services/Basket/Course.Services.Basket/Program.cs
@@ -1,26 +1,76 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Course.Services.Basket.Services;
+using Course.Services.Basket.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using System.IdentityModel.Tokens.Jwt;
 
-namespace Course.Services.Basket
+var builder = WebApplication.CreateBuilder(args);
+
+// Kimliği doğrulanmış kullanıcıyı verir.
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+builder.Services.AddControllers(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+// Identity içindeki Claim nesnelerinin Mapleme işlemini kaldırıyoruz.
+// NameIdentifier yerine sub olarak gelmesini sağladık kendi simlendirme işlemlerini es geçmesini sağladık. 
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    options.Authority = builder.Configuration["IdentityServerURL"];
+                    options.Audience = "resource_basket";
+                    options.RequireHttpsMetadata = true;
                 });
-    }
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+
+builder.Services.AddSingleton<RedisService>(sp =>
+{
+    // Configuration dosyalarını okuyoruz.
+    var settings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+
+    // Redis service oluşturuyoruz.
+    var redis = new RedisService(settings.Host, settings.Port);
+
+    // Redis'e bağlanıyor.
+    redis.Connect();
+
+    return redis;
+});
+
+builder.Services.AddScoped<IBasketService, BasketService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
